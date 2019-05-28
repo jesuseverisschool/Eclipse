e9b3fd737ea25025b8dcbd918e3bc33d9ba879eb
Luis Valero Molina <luisvaleromolina16@gmail.com>
2019-05-28 14:15:30 +0200

    Ya esta todo OK

--- Prueba/src/everis/eclipse/prueba/Prueba.java
+++ Prueba/src/everis/eclipse/prueba/Prueba.java
@@ -6,20 +6,32 @@ import java.util.Scanner;
 public class Prueba {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Introduce los datos del ordenador: ");
 		System.out.println("Introduce el modelo: ");
 		String modelo = sc.nextLine();
 		System.out.println("Introduce el número de serie: ");
 		String numeroSerie = sc.nextLine();
+		
 		Ordenador o = new Ordenador(modelo, numeroSerie);
+		
 		System.out.println("Datos del ordenador: " + o);
 		
-		System.out.println("Dime el nombre de la empresa: ");
-		String nombreEmpresa = scanner.nextLine();
+		//------------------------------------------------------------------------------------	
 		
+		System.out.println("Dime el nombre de la empresa: ");
+		String nombreEmpresa = sc.nextLine();
 		System.out.println("Dime la dirección de la empresa: ");
-		String direccionEmpresa = scanner.nextLine();
+		String direccionEmpresa = sc.nextLine();
+		
+		Empresa empresa = new Empresa (nombreEmpresa, direccionEmpresa);
+		
+		System.out.println(empresa);
+		
+		//------------------------------------------------------------------------------------
+		
 		System.out.print("Introduce el nombre del empleado: ");
 		String nombre = sc.nextLine();
 		System.out.print("Introduce el sueldo por hora: ");
@@ -27,10 +39,14 @@ public class Prueba {
 		System.out.print("Introduce el numero de horas trabajadas: ");
 		int numHorasTrab = sc.nextInt();
 		sc.nextLine();
+		
 		Empleado emp1 = new Empleado(nombre, sueldoHora, numHorasTrab);
+		
 		System.out.println(emp1);
 		
-		System.out.println("Introduzca nombre");
+		//------------------------------------------------------------------------------------
+		
+		System.out.println("Introduzca el nombre del departamento:");
 		String nombreDep = sc.nextLine();
 		System.out.println("Introduzca fecha año ");
 		int any = sc.nextInt();
@@ -38,14 +54,13 @@ public class Prueba {
 		int mes = sc.nextInt();
 		System.out.println("Introduzca fecha dia ");
 		int dia = sc.nextInt();
-		sc.close();
 		
-		Empresa empresa = new Empresa (nombreEmpresa, direccionEmpresa);
 		LocalDate fecha = LocalDate.of(any, mes, dia);
 		Departamento dp1 = new Departamento(nombreDep,fecha);
+		
 		System.out.println(dp1);
 		
-		System.out.println(empresa);
+		sc.close();
 	
-}
+	}
 }
