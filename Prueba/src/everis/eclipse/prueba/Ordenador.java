40be19bb9514365b7b5c06e749398972d5698b50
jesus <jesus.everisschool@gmail.com>
2019-06-04 13:33:52 +0200

    Modificado examen

--- Prueba/src/everis/eclipse/prueba/Ordenador.java
+++ Prueba/src/everis/eclipse/prueba/Ordenador.java
@@ -4,16 +4,18 @@ public class Ordenador {
 	
 	private String modelo;
 	private String numeroSerie;
+	private String marca;
 	
-	public Ordenador(String modelo, String numeroSerie) {
+	public Ordenador(String modelo, String numeroSerie, String marca) {
 		super();
 		this.modelo = modelo;
 		this.numeroSerie = numeroSerie;
+		this.marca = marca;
 	}
 	
 	@Override
 	public String toString() {
-		return modelo + " " + numeroSerie;
+		return modelo + " " + numeroSerie + " " + marca;
 	}
 	
 	
